c20c23c1b2a466e41994835db92ec01fc7009cfa
Marcus <marcus.henriksson@restory.ai>
2023-03-22 11:22:22 +0100

    Unpack Data Metod

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,7 @@
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
@@ -19,6 +23,8 @@ public class Main {
         try {
             //Kontrollera att Socket nummer är ledig. Avbryt om socket är upptagen
             serverSocket = new ServerSocket(4321);
+            System.out.println(serverSocket.getInetAddress());
+            System.out.println(serverSocket.getLocalSocketAddress());
         } catch (IOException e) {
             System.out.println(e);
             return;
@@ -60,5 +66,39 @@ public class Main {
         } finally {
             System.out.println("Server Avslutas");
         }
+    }
+
+    static String openUpData(String message) throws ParseException, IOException {
+        //Steg 1. Bygg upp JSON Obejct basserat på inkommande string
+        JSONParser parser = new JSONParser();
+        JSONObject jsonOb = (JSONObject) parser.parse(message);
+
+        //Steg 2. Läs av URL och HTTP-metod för att veta vad klienten vill
+        String url = jsonOb.get("httpURL").toString();
+        String method = jsonOb.get("httpMethod").toString();
+
+        //Steg 2.5. Dela upp URL med .split() metod
+        String[] urls = url.split("/");
+
+        //Steg 3. Använd en SwitchCase för att kolla vilken data som skall användas
+        switch (urls[0]) {
+            case "persons": {
+                if (method == "get") {
+                    //VIll hämta data om personer
+                    //TODO lägg till logik om det är specfik person som skall hämtas
+
+                    //Hämta data från JSON fil
+                    JSONObject jsonReturn = (JSONObject) parser.parse(new FileReader("data/data.json"));
+
+                    //Returnera JSON String
+                    return jsonReturn.toJSONString();
+
+                }
+                break;
+            }
+        }
+
+
+        return "message Recieved";
     }
 }
